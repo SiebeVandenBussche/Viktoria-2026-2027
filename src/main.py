ecb4abd2e8e1d2ff0b66123dbef3ef6288f5f205
siebevandenbussche <siebe.vandenbussche@scheppers-mechelen.be>
2026-05-05 12:55:05 +0200

    Switched to 4motor drivetrain

--- src/main.py
+++ src/main.py
@@ -1,31 +1,66 @@
 # ---------------------------------------------------------------------------- #
 #                                                                              #
-# 	Module:       main.py                                                      #
-# 	Author:       VandenBusscheSiebe                                           #
-# 	Created:      4/23/2026, 12:37:54 PM                                       #
-# 	Description:  V5 project                                                   #
+#   Module:       main.py                                                      #
+#   Author:       VandenBusscheSiebe                                           #
+#   Created:      4/23/2026, 12:37:54 PM                                       #
+#   Description:  V5 project                                                   #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
-
-# Library imports
 from vex import *
-
+# ---------------------------------------------------------------------------- #
+#   Devices                                                                    #
+# ---------------------------------------------------------------------------- #
 brain = Brain()
-
+controller = Controller()
+left  = MotorGroup(Motor(Ports.PORT1), Motor(Ports.PORT2))
+right = MotorGroup(Motor(Ports.PORT3), Motor(Ports.PORT4))
+# ---------------------------------------------------------------------------- #
+#   Drive functions                                                            #
+# ---------------------------------------------------------------------------- #
+k = 2
+def driveGraph(x):
+    """
+    A simple mathemathic function to translate controller input into velocity output.
+    """
+    if x > 0:
+        return (x**k)/10**((k-1)*2)
+    else:
+        return -(x**k)/10**((k-1)*2)
+def changeDriveGraph(controller: Controller):
+    """
+    changes the constant in the DriveGraph on button press
+    """
+    global k
+    if controller.buttonUp.pressed:
+        k+=1
+    elif controller.buttonDown.pressed:
+        k-=1
+    controller.screen.clear_screen()
+    controller.screen.print(k)
+def arcadeDriveGraph(left: MotorGroup, right: MotorGroup, controller: Controller):
+    """
+    Arcade drive using the left joystick for forward/backward movement and the right joystick for turning.
+    a graph translates forward controller input to forward speed.
+    """
+    left.set_velocity((driveGraph(controller.axis3.position()) + controller.axis1.position()), PERCENT)
+    right.set_velocity((driveGraph(controller.axis3.position()) - controller.axis1.position()), PERCENT)
+    left.spin(FORWARD)
+    right.spin(FORWARD)
+# ---------------------------------------------------------------------------- #
+#   Competition                                                                #
+# ---------------------------------------------------------------------------- #
 def autonomous():
     brain.screen.clear_screen()
-    brain.screen.print("autonomous code")
-    # place automonous code here
-
+    brain.screen.print("Autonomous")
+    # place autonomous code here
 def user_control():
     brain.screen.clear_screen()
-    brain.screen.print("driver control")
-    # place driver control in this while loop
+    brain.screen.print("Driver Control")
     while True:
+        arcadeDriveGraph(left, right, controller)
         wait(20, MSEC)
-
-# create competition instance
+# ---------------------------------------------------------------------------- #
+#   Other thing                                                                #
+# ---------------------------------------------------------------------------- #
 comp = Competition(user_control, autonomous)
-
-# actions to do when the program starts
 brain.screen.clear_screen()
